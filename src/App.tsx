45ec0e87953cd0a8f9e0915c72e6dd4a65e0dfff
Friends233 <Friends233@foxmail.com>
2023-07-03 16:01:55 +0800

    github改造

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import GameTest from "./game/game";
+
+const urlPre = location.href.includes("friends233.github.io")
+  ? "/cocos-game"
+  : "";
 const GameFrameUrlList = [
   {
-    url: "/src/cocos1010/index.html",
+    url: urlPre + "/src/cocos1010/index.html",
     name: "1010",
-    img: "/src/cocos1010/1010.png",
+    img: urlPre + "/src/cocos1010/1010.png",
   },
   {
-    url: "/src/cocosGoldenMiner/index.html",
+    url: urlPre + "/src/cocosGoldenMiner/index.html",
     name: "黄金矿工",
-    img: "/src/cocosGoldenMiner/goldenMiner.png",
+    img: urlPre + "/src/cocosGoldenMiner/goldenMiner.png",
   },
 ];
 
@@ -18,7 +22,9 @@ function App() {
   const [gameFrameUrl, setGameFrameUrl] = useState("");
   return (
     <div>
-      <h4 className="game-title">欢迎来到Friends233的小游戏，请用手机打开预览</h4>
+      <h4 className="game-title">
+        欢迎来到Friends233的小游戏，请用手机打开预览
+      </h4>
       {gameFrameUrl ? (
         <GameTest url={gameFrameUrl} closeGame={() => setGameFrameUrl("")} />
       ) : (
