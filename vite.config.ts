6f5dca19990aafab6240af33e19ad1a0550d559b
Friends233 <friends233@foxmail.com>
2025-04-27 15:51:59 +0800

    feat: 调整vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    outDir:'docs',
+  build: {
+    outDir: 'docs',
   },
+  base: '/cocos-game/',
   plugins: [react()],
-})
+});
